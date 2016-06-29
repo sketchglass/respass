d91b2e26a94ee22962e67ee2ffc96e40d8a94a70
minamorl <minamorl@users.noreply.github.com>
2016-06-29 15:24:31 +0900

    show latest 10 comments

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,28 @@ let sequelize = new Sequelize('sample','','',{dialect:'sqlite',storage:'./sample
 let Message = sequelize.define('message', {
   text: Sequelize.STRING,
 });
-Message.sync()
+Message.sync({
+  // !! breaks current db (do not use in production)
+  // force: true
+})
 
 
 let wss = new Server({port: 8080})
 wss.on('connection', (ws) => {
   ws.send("starting connection")
+  // send latest 10 messages
+  Message.findAll({
+    limit: 10,
+    order: "id DESC",
+    raw: true
+  }).then((messages) => {
+    messages.reverse()
+    console.log(messages)
+    messages.forEach((obj) => {
+      ws.send(obj["text"])
+    })
+  })
+
   ws.on('message', (message) => {
     ws.send("received:" + message )
     Message.create({text: message}).then(() => {
