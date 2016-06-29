26a55d627ae88a25d937190e8500b41dc45cc611
minamorl <minamorl@users.noreply.github.com>
2016-06-29 22:57:38 +0900

    login/logout notification

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,14 @@ let findLatestMessage = (num: number) => {
 
 enum ReceiveEventType {
   CREATE_MESSAGE,
+  JOIN,
   PONG,
+  LEFT,
 }
 enum SendEventType {
   NEW_MESSAGE,
+  USER_JOIN,
+  USER_LEAVE,
   PING,
 }
 
@@ -29,6 +33,16 @@ class CreateMessageEvent extends BaseReceiveEvent{
   response() {
     Message.create({text: this.value})
     return newMessage(SendEventType.NEW_MESSAGE, this.value) 
+  }
+}
+class JoinEvent extends BaseReceiveEvent {
+  response() {
+    return newMessage(SendEventType.USER_JOIN, this.value)
+  }
+}
+class LeftEvent extends BaseReceiveEvent {
+  response() {
+    return newMessage(SendEventType.USER_LEAVE, this.value)
   }
 }
 let newMessage = (ev: SendEventType, value: string) => {
@@ -45,9 +59,12 @@ export let bootup = () => {
       client.send(message)
     })
   }
-
-
+  
   wss.on('connection', (ws) => {
+    // join event
+    let response = new JoinEvent(ReceiveEventType.JOIN, "user joined").response()
+    broadcast(response)
+
     // send latest 10 messages
     findLatestMessage(10).then((messages) => {
       messages.forEach((obj) => {
@@ -55,10 +72,11 @@ export let bootup = () => {
       })
     })
 
-    // wip
+    // wip (below is broken)
     // setInterval(() => {
       // ws.send(newMessage(SendEventType.PING, ""))
     // }, 2000)
+
     /*
      * expected json shceme:
      * {
@@ -75,12 +93,19 @@ export let bootup = () => {
     ws.on('message', (undecoded_json: string) => {
       try {
         let json = JSON.parse(undecoded_json)
-        // switching
         let {ev, value} = json
         if (ev === ReceiveEventType[ReceiveEventType.CREATE_MESSAGE]) {
           let response = new CreateMessageEvent(ev, value).response()
           broadcast(response)
         }
+      } catch(e) {
+        // failed
+      }
+    })
+    ws.on('close', () => {
+      let response = new LeftEvent(ReceiveEventType.LEFT, "user left").response()
+      try {
+        broadcast(response)
       } catch(e) {
         // failed
       }
