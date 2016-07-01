5e6258b2af2b77d7648b029b87c093a9bc818eb5
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-01 23:46:57 +0900

    Return IMessage array

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,21 @@
 import * as http from "http";
 import * as express from "express";
-import {Message} from "./models";
+import {Message, User} from "./models";
+import {IMessage} from "../common/data";
 
 export const server = http.createServer();
 const app = express();
 
 app.get("/messages", async (req, res) => {
-  const messages = await Message.findAll({
-    order: "createdAt",
-    raw: true
+  const messages: any[] = await Message.findAll({
+    include: [User],
+    //order: "createdAt", <- this doesn't work ???
   });
-  res.json(messages);
+  const data: IMessage[] = messages.map(m => ({
+    text: m.text,
+    user: {name: m.user.name}
+  }));
+  res.json(data);
 });
 
 server.on("request", app);
