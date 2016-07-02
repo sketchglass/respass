dc8a3db56b6846d8e93cf092835c77ec67c0e84b
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-02 21:34:08 +0900

    Install passport

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,6 +3,7 @@
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/passport/index.d.ts" />
 /// <reference path="globals/power-assert-formatter/index.d.ts" />
 /// <reference path="globals/power-assert/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
