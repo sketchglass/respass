cd05eccbc6069da08bdde79c453caf8fcec42615
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-02 23:10:24 +0900

    Fix sync order

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { Message, User, TwitterIntegration } from "./models"
 let IS_PROD: boolean = process.env.NODE_ENV === "production"
 
 // define models
-for (const model of [Message, User, TwitterIntegration]) {
+for (const model of [Message, TwitterIntegration, User]) {
   model.sync({
     // !! breaks current db (do not use in production)
     force: !IS_PROD
