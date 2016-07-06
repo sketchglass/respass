00e9985d02261ef54e90a9ad2821713037e337a5
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-06 20:18:31 +0900

    Fix column name

--- migrations/20160706063856-user-icon.ts
+++ migrations/20160706063856-user-icon.ts
@@ -2,10 +2,10 @@ import * as Sequelize from "sequelize"
 
 const migration = {
   up: async (queryInterface: Sequelize.QueryInterface) => {
-    await queryInterface.addColumn("Users", "iconUrl", Sequelize.STRING)
+    await queryInterface.addColumn("users", "iconUrl", Sequelize.STRING)
   },
   down: async (queryInterface: Sequelize.QueryInterface) => {
-    await queryInterface.removeColumn("Users", "iconUrl")
+    await queryInterface.removeColumn("users", "iconUrl")
   }
 }
 
