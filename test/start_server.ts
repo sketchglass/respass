a1d4294e0e058de1b9280b209ea7260a13ff6223
minamorl <minamorl@users.noreply.github.com>
2016-07-08 15:03:38 +0900

    Revert "create websocket server per test"
    This reverts commit e603ba6d3a466d887c1275cb574a4d91092c1c96.

--- test/start_server.ts
+++ test/start_server.ts
@@ -3,3 +3,11 @@ process.env.SESSION_SECRET = "dummy"
 process.env.TWITTER_CONSUMER_KEY = "dummy"
 process.env.TWITTER_CONSUMER_SECRET = "dummy"
 process.env.TWITTER_CALLBACK_URL = "dummy"
+
+import {server} from "../server/app"
+
+before(done => {
+  server.listen(0, () => {
+    done()
+  })
+})
