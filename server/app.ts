7b4270ba52d14b9d79912086fbf9b831ceebe107
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-08 15:49:21 +0900

    Warn on unhandled errors

--- server/app.ts
+++ server/app.ts
@@ -5,5 +5,13 @@ import { Message, User, TwitterIntegration, Connection } from "./models"
 // Connection must be cleaned up after restart server
 Connection.destroy({where: {}})
 
+process.on('uncaughtException', (err: any) => {
+  console.warn(`Caught exception: ${err}`);
+})
+
+process.on('unhandledRejection', (reason: any, p: Promise<any>) => {
+    console.warn("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+})
+
 export const app = express()
 export const server = http.createServer(app)
