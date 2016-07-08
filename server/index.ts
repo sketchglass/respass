d5466ae1646a7a5806e0a13ca9f8824fdbedb070
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-08 10:38:28 +0900

    add server.ts

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,5 @@
-import * as express from "express"
-import * as path from "path"
-import { Message, User, TwitterIntegration, Connection } from "./models"
-
 require('dotenv').config();
 
-// Connection must be cleaned up after restart server
-Connection.destroy({where: {}})
+import {listen} from "./server"
 
-import { app } from "./app"
-app.use(express.static(path.join(__dirname, '../../ui/dist')))
-import "./auth"
-import "./api"
-import "./wsserver"
-
-app.listen(process.env.PORT);
+listen(process.env.PORT)
