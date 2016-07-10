89c9f1cabd191b5ce5ec3dadf8e99b38d5bab40f
minamorl <minamorl@users.noreply.github.com>
2016-07-11 03:27:22 +0900

    fix

--- ui/src/mobile.ts
+++ ui/src/mobile.ts
@@ -1,22 +1,23 @@
 import * as Hammer from "hammerjs"
 
-// create overlay
-const element = document.createElement("div")
-element.style.width = "400px"
-element.style.height = "100%"
-element.style.zIndex = "100"
-element.style.top = "0"
-element.style.left = "0"
-element.style.position = "fixed"
-document.body.appendChild(element)
+if(window.matchMedia("(max-width: 500px)").matches) {
+  // create overlay
+  const element = document.createElement("div")
+  element.style.width = "400px"
+  element.style.height = "100%"
+  element.style.zIndex = "100"
+  element.style.top = "0"
+  element.style.left = "0"
+  element.style.position = "fixed"
+  document.body.appendChild(element)
 
-const mc = new Hammer(element)
-let visibility = false
-mc.on("swiperight", () => {
-  let userList = <HTMLElement>document.querySelector(".user-list")
-  userList.style.display = "flex"
-})
-mc.on("swipeleft", () => {
-  let userList = <HTMLElement>document.querySelector(".user-list")
-  userList.style.display = "none"
-})
+  const mc = new Hammer(element)
+  mc.on("swiperight", () => {
+    let userList = <HTMLElement>document.querySelector(".user-list")
+    userList.style.display = "flex"
+  })
+  mc.on("swipeleft", () => {
+    let userList = <HTMLElement>document.querySelector(".user-list")
+    userList.style.display = "none"
+  })
+}
