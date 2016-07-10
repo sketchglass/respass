7e461f1c7f947a1ddb5e78344c44f17d7f14101b
minamorl <minamorl@users.noreply.github.com>
2016-07-11 04:08:32 +0900

    quickpatch

--- ui/src/mobile.ts
+++ ui/src/mobile.ts
@@ -3,9 +3,9 @@ import * as Hammer from "hammerjs"
 if(window.matchMedia("(max-width: 500px)").matches) {
   // create overlay
   const element = document.createElement("div")
-  element.style.width = "400px"
+  element.style.width = "300px"
   element.style.height = "100%"
-  element.style.zIndex = "100"
+  element.style.zIndex = "2"
   element.style.top = "0"
   element.style.left = "0"
   element.style.position = "fixed"
@@ -13,11 +13,15 @@ if(window.matchMedia("(max-width: 500px)").matches) {
 
   const mc = new Hammer(element)
   mc.on("swiperight", () => {
-    let userList = <HTMLElement>document.querySelector(".user-list")
+    const messageForm = <HTMLElement>document.querySelector(".message-form")
+    messageForm.style.zIndex = "0"
+    const userList = <HTMLElement>document.querySelector(".user-list")
     userList.style.display = "flex"
   })
   mc.on("swipeleft", () => {
-    let userList = <HTMLElement>document.querySelector(".user-list")
+    const messageForm = <HTMLElement>document.querySelector(".message-form")
+    messageForm.style.zIndex = "4"
+    const userList = <HTMLElement>document.querySelector(".user-list")
     userList.style.display = "none"
   })
 }
