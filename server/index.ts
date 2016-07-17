6cb0a370f2c22527578187d54b18e00d9dc31e08
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-17 18:07:58 +0900

    Revert "Redirect http to https"

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,7 @@ require('dotenv').config();
 import * as path from "path"
 import * as express from "express"
 import {app, server} from "./app"
-const enforce = require('express-sslify')
 
-if (process.env.NODE_ENV !== "development") {
-  app.use(enforce.HTTPS({trustProtoHeader: true}))
-}
 app.use(express.static(path.join(__dirname, '../../ui/dist')))
 import "./auth"
 import "./api"
