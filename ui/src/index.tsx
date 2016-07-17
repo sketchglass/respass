5b9c64a64fb507c21d76d37652639e94185d4c61
Ryohei Ikegami <iofg2100@gmail.com>
2016-07-17 11:18:35 +0900

    Add Google Analytics

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -10,3 +10,18 @@ import ThreadView from "./views/ThreadView";
 window.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<ThreadView></ThreadView>, document.getElementById("app"));
 });
+
+const {GA_TRACKING_ID} = process.env
+if (GA_TRACKING_ID) {
+  const script = document.createElement("script")
+  script.innerHTML = `
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', ${JSON.stringify(GA_TRACKING_ID)}, 'auto');
+    ga('send', 'pageview');
+  `
+  document.body.appendChild(script)
+}
